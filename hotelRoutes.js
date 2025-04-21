78cf101ea124e5ffeac5541ebeef2f6f9f2176c8
ekinsahin <120283920+ekinsahin@users.noreply.github.com>
2025-04-21 22:44:07 +0300

    Create hotelRoutes.js
    backend sistemi projeye eklendi

---
+++ hotelRoutes.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const Hotel = require('../models/Hotel');
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+  try {
+    const hotels = await Hotel.find();
+    res.json(hotels);
+  } catch (error) {
+    res.status(500).json({ message: 'Error fetching hotels' });
+  }
+});
+
+module.exports = router;
